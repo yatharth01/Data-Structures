234c972a0d2e42a79b0ae5e6018fb0d5a08a76d4
yatharth01 <yatharthmishra01@gmail.com>
2018-07-13 14:36:07 +0530

    Insertion methods Added

--- LinkedList-simple.cpp
+++ LinkedList-simple.cpp
@@ -6,6 +6,60 @@ struct Node{
 	struct Node *next;
 };
 
+
+void insert_node_at_front(Node **head , int new_data)
+{
+	Node *new_node = (struct Node*)malloc(sizeof(struct Node));
+	new_node->data=new_data;
+	new_node->next= *head;
+
+    *head = new_node;
+}
+
+void insert_node_after(Node *n,int new_data)
+{
+     if (n== NULL) 
+    { 
+       printf("previous node is NULL");       
+       return;  
+    } 
+
+	Node *new_node=(struct Node*)malloc(sizeof(struct Node));
+
+	new_node->data=new_data;
+	new_node->next= n->next; 
+	n->next = new_node;
+
+}
+
+void insert_node_at_end(Node **head,int new_data)
+{
+   struct Node *new_node= (struct Node*)malloc(sizeof(struct Node));
+
+   struct Node *last = *head;
+
+   new_node->data= new_data;
+   
+   new_node->next=NULL;
+   
+   if(*head==NULL)
+   {
+   	*head=new_node;
+   	return;
+   }
+
+   else
+   {
+   	while( last->next !=NULL)
+   	{
+   		last= last->next;
+   	}
+    
+    last->next=new_node;
+   }
+
+}
+
 void print(struct Node *n)
 {
 	while(n!=NULL)
@@ -35,7 +89,11 @@ int main()
 
    third->data=3;
    third->next=NULL;
+   
+   insert_node_at_front(&head,7);
+   insert_node_after(head->next,9);
 
+   insert_node_at_end(&head,15);
    print(head);
 
 }
